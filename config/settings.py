bb8fcb446d4d0dc3f62a04a45d36d30fb40a5112
Sarah Beverton <sebryant@gmail.com>
2021-01-07 14:28:24 -0500

    added responsive rows of pins to boards and a way to get to the boards on the profile page

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '84-7oqxj7i^hf*#naze*&$94u)oj)818n&kn8@oo#(m4ah%2w#'
+SECRET_KEY="84-7oqxj7i^hf*#naze*&$94u)oj)818n&kn8@oo#(m4ah%2w#"
+#SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
