93abc9f0b523cd906d318d8a7489b73f639ceb4c
David Guzman <davidguzmaninc@gmail.com>
2021-01-09 00:41:24 -0500

    Added a profile model

--- pinusers/models.py
+++ pinusers/models.py
@@ -4,5 +4,15 @@ from pins.models import Pin
 
 # Create your models here.
 
+
 class PinUser(AbstractUser):
     pins = models.ManyToManyField(Pin, blank=True, symmetrical=False)
+
+
+class Profile(models.Model):
+    user_following = models.ForeignKey(
+        PinUser, related_name='users_following', null=True, on_delete=models.CASCADE)
+    user_followers = models.ForeignKey(
+        PinUser,
+        related_name='user_followers', on_delete=models.CASCADE
+    )
