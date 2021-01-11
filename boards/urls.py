637aea0c9d2d81032b352c9f4fbb5e8140a0611c
Sarah Beverton <sebryant@gmail.com>
2021-01-10 19:29:07 -0500

    moved Add Board link to navbar instead of profile page

--- boards/urls.py
+++ boards/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from boards import views
 
 urlpatterns = [
-    path('addboard/', views.AddBoardView.as_view()),
+    path('addboard/', views.AddBoardView.as_view(), name="addboard"),
     path('<int:board_id>/', views.BoardView.as_view(), name="board"),
     path('<int:board_id>/save/<int:pin_id>', views.SaveToBoardView.as_view()),
 ]
