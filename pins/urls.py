7d08c5906bd69774328e6e450056f1382f0fa81f
Sarah Beverton <sebryant@gmail.com>
2021-01-14 13:34:58 -0500

    added way to remove a pin from a user and their board if they saved it to one

--- pins/urls.py
+++ pins/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('addpin/', views.AddPinView.as_view(), name="addpin"),
     path('<int:pin_id>/', views.PinView.as_view(), name="pin_detail"),
     path('save/<int:pin_id>/', views.SaveView.as_view()),
+    path('remove/<int:pin_id>/', views.RemovePinView.as_view()),
     path('like/<int:pin_id>/', views.like_view)
 ]
